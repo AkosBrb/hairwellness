7dd26e54942e470c9446d31b29b8754264516dd2
akos borbely <macos.borbely@gmail.com>
2024-10-10 07:37:06 +0200

    create products context

---
+++ salon-website/src/contexts/ProductsContexts.js
@@ -0,0 +1,9 @@
+import { createContext, useState } from "react";
+
+export const ProductsContext = createContext();
+
+export function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  return <ProductsContext.Provider value={{ products, setProducts }}>{children}</ProductsContext.Provider>
+}

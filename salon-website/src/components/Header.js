278839965958b5a0ad4705f22e6a0416ab8d3a3d
akos borbely <macos.borbely@gmail.com>
2024-10-10 07:36:13 +0200

    header redesign

--- salon-website/src/components/Header.js
+++ salon-website/src/components/Header.js
@@ -1,28 +1,38 @@
-import brandLogo from "../assets/img/logo.jpg"
-import facebookLogo from "../assets/img/face.png"
-import instaLogo from "../assets/img/insta.png"
 import "../Style.css"
+import { MdOutlineFacebook } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa";
+import { IoLogoTiktok } from "react-icons/io5";
 import { NavLink } from "react-router-dom"
 
 function Header() {
     return (
         <div className="header--background">
             <header className="header--container">
-                <img alt="Logo.." src={brandLogo} id="brand--logo"></img>
-                <div className="nav--title--container">
-                    <h1 className="main--title">Hajwellness & fodrászat</h1>
-                    <nav className="navbar--container">
-                        <ul className="list--container">
-                            <li className="nav--link"><NavLink to={"/"}>Főoldal</NavLink></li>
-                            <li className="nav--link"><NavLink to={"/galeria"}>Galéria</NavLink></li>
-                            <li className="nav--link"><NavLink to={"/arak"}>Árak</NavLink></li>
-                            <li className="nav--link"><NavLink to={"/kapcsolat"}>Kapcsolat</NavLink></li>
-                        </ul>
-                    </nav>
+                <div className="first--header--container">
+                    <div className="contact--infos">
+                        <span>+ 36 70 266 2221</span>
+                        <span>hajwellness@hajwellness.hu</span>
+                        <span>2510 Dorog, Bécsi út 53.</span>
+                    </div>
+                    <div className="social--container">
+                        <a href="https://www.facebook.com/profile.php?id=100090432196857"><MdOutlineFacebook /></a>
+                        <a href="/"><FaInstagram /></a>
+                        <a href="/"><IoLogoTiktok /></a>
+                    </div>
                 </div>
-                <div className="social--container">
-                    <a href="https://www.facebook.com/profile.php?id=100090432196857"><img alt="facebook logo" src={facebookLogo} className="facebook social--logo" /></a>
-                    <img src={instaLogo} alt="instagram logo" className="instagram social--logo" />
+                <hr />
+                <div className="second--header--container">
+                    <div className="brand--logo">LOGO</div>
+                    <div className="nav--title--container">
+                        <nav className="navbar--container">
+                            <ul className="list--container">
+                                <li className="nav--link"><NavLink to={"/"}>Főoldal</NavLink></li>
+                                <li className="nav--link"><NavLink to={"/galeria"}>Termékek</NavLink></li>
+                                <li className="nav--link"><NavLink to={"/szolgaltatasok"}>Szolgáltatások</NavLink></li>
+                                <li className="nav--link"><NavLink to={"/kapcsolat"}>Kapcsolat</NavLink></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </header>
             <div className="header--blur"></div>

a52265894d16338bf15639280314658a185d4151
akos borbely <macos.borbely@gmail.com>
2024-10-11 07:18:53 +0200

    made inputs controlled with form data state

--- salon-website/src/components/Contact.js
+++ salon-website/src/components/Contact.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 function Contact() {
+    const [formData, setFormData] = useState({
+        name: "",
+        phone: "",
+        email: "",
+        message: ""
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
     return (
         <section className="contact--page--container">
             <div className="form--container">
                 <h2>Kérdése van? írjon üzenetet!</h2>
                 <form onSubmit={(e) => e.preventDefault()} className="contact--form">
-                    <input required type="text" placeholder="Név" />
-                    <input required type="tel" placeholder="Telefonszám" />
-                    <input required type="email" placeholder="Email" />
-                    <input required type="text" placeholder="Üzenet" />
-                    <button className="btn">Küldés</button>
+                    <input onChange={(e) => handleChange(e)} required name={"name"} type="text" placeholder="Név" />
+                    <input onChange={(e) => handleChange(e)} required name={"phone"} type="tel" placeholder="Telefonszám" />
+                    <input onChange={(e) => handleChange(e)} required name={"email"} type="email" placeholder="Email" />
+                    <input onChange={(e) => handleChange(e)} required name={"message"} type="text" placeholder="Üzenet" />
+                    <button className="btn" type="submit">Küldés</button>
                 </form>
             </div>
         </section>

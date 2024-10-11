fbc14fd42112ef9e0705e354945700f369f0be05
akos borbely <macos.borbely@gmail.com>
2024-10-11 06:58:37 +0200

    create contact form

--- salon-website/src/components/Contact.js
+++ salon-website/src/components/Contact.js
@@ -1,6 +1,17 @@
 function Contact() {
     return (
-        <h1>Kapcsolat</h1>
+        <section className="contact--page--container">
+            <div className="form--container">
+                <h2>Kérdése van? írjon üzenetet!</h2>
+                <form onSubmit={(e) => e.preventDefault()} className="contact--form">
+                    <input required type="text" placeholder="Név" />
+                    <input required type="tel" placeholder="Telefonszám" />
+                    <input required type="email" placeholder="Email" />
+                    <input required type="text" placeholder="Üzenet" />
+                    <button className="btn">Küldés</button>
+                </form>
+            </div>
+        </section>
     )
 }
 

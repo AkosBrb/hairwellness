01e45586eeaf492ec941c26153b7db4112d85a2b
akos borbely <macos.borbely@gmail.com>
2024-10-15 07:34:26 +0200

    implement sendEmail

--- salon-website/src/components/Contact.js
+++ salon-website/src/components/Contact.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { sendEmail } from "../services/sendEmail";
 
 function Contact() {
     const [formData, setFormData] = useState({
@@ -13,15 +14,22 @@ function Contact() {
         setFormData({ ...formData, [name]: value });
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        sendEmail(formData);
+    }
+
+    console.log(formData)
+
     return (
         <section className="contact--page--container">
             <div className="form--container">
                 <h2>Kérdése van? írjon üzenetet!</h2>
-                <form onSubmit={(e) => e.preventDefault()} className="contact--form">
+                <form onSubmit={(e) => handleSubmit(e)} className="contact--form">
                     <input onChange={(e) => handleChange(e)} required name={"name"} type="text" placeholder="Név" />
                     <input onChange={(e) => handleChange(e)} required name={"phone"} type="tel" placeholder="Telefonszám" />
                     <input onChange={(e) => handleChange(e)} required name={"email"} type="email" placeholder="Email" />
-                    <input onChange={(e) => handleChange(e)} required name={"message"} type="text" placeholder="Üzenet" />
+                    <textarea cols={30} rows={10} onChange={(e) => handleChange(e)} required name={"message"} placeholder="Üzenet" />
                     <button className="btn" type="submit">Küldés</button>
                 </form>
             </div>

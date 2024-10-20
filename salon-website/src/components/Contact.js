6e20900456e3ffb5954e0194a271a95788933737
akos borbely <macos.borbely@gmail.com>
2024-10-20 18:04:44 +0200

    modify classnames and insert img

--- salon-website/src/components/Contact.js
+++ salon-website/src/components/Contact.js
@@ -1,5 +1,6 @@
 import { sendEmail } from "../services/sendEmail";
 import { useFormik } from 'formik';
+import contactImg from '../assets/img/blonde-female-getting-new-hairstyle-hair-salon.jpg';
 
 function Contact() {
 
@@ -17,35 +18,46 @@ function Contact() {
     })
 
     return (
-        <section className="contact--page--container">
-            <div className="contact--header">
-                <h2>Kérdése van? írjon üzenetet!</h2>
-                <div className="contact--page--infos--container">
-                    <h4>CÍM:</h4>
-                    <p>2510 Dorog</p>
-                    <p>Bécsi út 53.</p>
+        <section className="contact--page">
+            <div className="container">
+                <div className="form--container">
+                    <h2>Kérdése van? Írjon üzenetet!</h2>
+                    <div className="contact--header">
+                        <div className="contact--info--container">
+                            <h4>CÍM:</h4>
+                            <p>2510 Dorog</p>
+                            <p>Bécsi út 53.</p>
+                        </div>
+                        <div className="contact--info--container">
+                            <h4>EMAIL:</h4>
+                            <p>hajwellness@hajwellness.hu</p>
+                        </div>
+                        <div className="contact--info--container">
+                            <h4>TELEFON:</h4>
+                            <p>+ 36 70 266 2221</p>
+                        </div>
+                    </div>
+                    <form onSubmit={(e) => formik.handleSubmit(e)} className="contact--form">
+                        <div className="input--container">
+                            <label htmlFor="name-input">Név</label>
+                            <input onChange={formik.handleChange} value={formik.values.name} id="name-input" name="name" type="text" />
+                        </div>
+                        <div className="input--container">
+                            <label htmlFor="phone-input">Telefonszám</label>
+                            <input onChange={formik.handleChange} value={formik.values.phone} id="phone-input" name="phone" type="tel" />
+                        </div>
+                        <div className="input--container">
+                            <label htmlFor="email-input">Email cím</label>
+                            <input onChange={formik.handleChange} value={formik.values.email} id="email-input" name="email" type="email" />
+                        </div>
+                        <div className="input--container textarea--container">
+                            <label htmlFor="message-input">Üzenet</label>
+                            <textarea onChange={formik.handleChange} value={formik.values.message} id="message-input" name="message" />
+                        </div>
+                        <button className="btn" type="submit">Küldés</button>
+                    </form>
                 </div>
-            </div>
-            <div className="form--container">
-                <form onSubmit={(e) => formik.handleSubmit(e)} className="contact--form">
-                    <div className="name--input">
-                        <label htmlFor="name-input">Név</label>
-                        <input onChange={formik.handleChange} value={formik.values.name} id="name-input" name="name" type="text" />
-                    </div>
-                    <div className="phone--input">
-                        <label htmlFor="phone-input">Telefonszám</label>
-                        <input onChange={formik.handleChange} value={formik.values.phone} id="phone-input" name="phone" type="tel" />
-                    </div>
-                    <div className="email--input--container">
-                        <label htmlFor="email-input">Email cím</label>
-                        <input onChange={formik.handleChange} value={formik.values.email} id="email-input" name="email" type="email" />
-                    </div>
-                    <div className="message--input--container">
-                        <label htmlFor="message-input">Üzenet</label>
-                        <textarea cols={30} rows={10} onChange={formik.handleChange} value={formik.values.message} id="message-input" name="message" />
-                    </div>
-                    <button className="btn" type="submit">Küldés</button>
-                </form>
+                <img src={contactImg} alt="contact-img" />
             </div>
         </section>
     )
--- salon-website/src/components/Services.js
+++ salon-website/src/components/Services.js
@@ -8,28 +8,28 @@ import kid from "../assets/img/kid.jpg"
 import womanService from "../assets/img/shutterstock_1452287459.jpg"
 function Services() {
     return (
-        <main className="main--container">
+        <main className="services--container">
             <img alt="IMG" className="service--img" src={laminal}></img>
             <div className="service--one">
-                <img className="main--icon" alt="Kép helye.." src={chair} />
+                <img className="service--icon" alt="Kép helye.." src={chair} />
                 <h2>Hajszerkezet újjáépítés</h2>
                 <p>1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, voluptatum temporibus quidem perspiciatis dolorem libero corrupti perferendis molestiae quibusdam odit. Odit eveniet commodi quidem adipisci. Architecto nobis laudantium cumque placeat dolorem sunt autem cum illum, eius rerum fugiat voluptates, numquam natus? Ut dolores distinctio recusandae cum officiis molestiae ipsa adipisci!</p>
             </div>
             <img alt="IMG" className="service--img" src={men}></img>
             <div className="service--two">
-                <img className="main--icon" alt="Kép helye.." src={cutting} />
+                <img className="service--icon" alt="Kép helye.." src={cutting} />
                 <h2>Férfi hajvágás</h2>
                 <p>2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, voluptatum temporibus quidem perspiciatis dolorem libero corrupti perferendis molestiae quibusdam odit. Odit eveniet commodi quidem adipisci. Architecto nobis laudantium cumque placeat dolorem sunt autem cum illum, eius rerum fugiat voluptates, numquam natus? Ut dolores distinctio recusandae cum officiis molestiae ipsa adipisci!</p>
             </div>
             <img alt="IMG" className="service--img" src={kid}></img>
             <div className="service--three">
-                <img className="main--icon" alt="Kép helye.." src={woman} />
+                <img className="service--icon" alt="Kép helye.." src={woman} />
                 <h2>Gyermek hajvágás</h2>
                 <p>3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, voluptatum temporibus quidem perspiciatis dolorem libero corrupti perferendis molestiae quibusdam odit. Odit eveniet commodi quidem adipisci. Architecto nobis laudantium cumque placeat dolorem sunt autem cum illum, eius rerum fugiat voluptates, numquam natus? Ut dolores distinctio recusandae cum officiis molestiae ipsa adipisci!</p>
             </div>
             <img alt="IMG" className="service--img" src={womanService}></img>
             <div className="service--four">
-                <img className="main--icon" alt="Kép helye.." src={scissor} />
+                <img className="service--icon" alt="Kép helye.." src={scissor} />
                 <h2>Hajgyógyászati kezelés</h2>
                 <p>4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, voluptatum temporibus quidem perspiciatis dolorem libero corrupti perferendis molestiae quibusdam odit. Odit eveniet commodi quidem adipisci. Architecto nobis laudantium cumque placeat dolorem sunt autem cum illum, eius rerum fugiat voluptates, numquam natus? Ut dolores distinctio recusandae cum officiis molestiae ipsa adipisci!</p>
             </div>

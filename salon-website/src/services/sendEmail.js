d5de2707322ba82c46bbc6b0f5cb1082cf43f587
akos borbely <macos.borbely@gmail.com>
2024-10-22 07:51:08 +0200

    send payload changed and env variables implement

--- salon-website/src/services/sendEmail.js
+++ salon-website/src/services/sendEmail.js
@@ -1,16 +1,16 @@
 import emailjs from '@emailjs/browser';
+import { SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY } from '../constants/emailJsConfig';
 
 export function sendEmail({ name, phone, email, message }) {
   const templateParams = {
     from_name: name,
-    from_email: email,
-    to_name: '',
-    message: message
+    data: {
+      email,
+      phone,
+      message
+    }
   }
-  const serviceID = 'service_7b5m63g';
-  const templateID = 'template_qkd9bbj';
-  const publicKey = '3oHOX7x9cCWpob_5y';
-  emailjs.send(serviceID, templateID, templateParams, publicKey).then(
+  emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY).then(
     (response) => {
       console.log('SUCCESS!', response.status, response.text);
     },

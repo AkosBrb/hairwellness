645335bbf86c6163239e6717887480dce35fe50c
akos borbely <macos.borbely@gmail.com>
2024-10-25 11:50:35 +0200

    text changed

--- salon-website/src/components/MessageSent.js
+++ salon-website/src/components/MessageSent.js
@@ -1,6 +1,6 @@
 function MessageSent() {
   return (
-    <div className="message--sent--container"><h2>Köszönjük az üzenete elküldésre került! Hamarosan felveszem Önnel a kapcsolatot.</h2></div>
+    <div className="message--sent--container"><h2>Köszönöm, az üzenete elküldésre került! Hamarosan felveszem Önnel a kapcsolatot.</h2></div>
   )
 }
 

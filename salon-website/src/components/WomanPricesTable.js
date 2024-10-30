34929d3818181d92f861ecd0903962240218a1f7
akos borbely <macos.borbely@gmail.com>
2024-10-30 07:54:58 +0100

    remove inherited data

--- salon-website/src/components/WomanPricesTable.js
+++ salon-website/src/components/WomanPricesTable.js
@@ -1,28 +1,6 @@
 function WomanPricesTable() {
   return (
-    <section className="prices--container">
-      <h2>Árak:</h2>
-      <div className="underline"></div>
-      <table>
-        <thead>
-          <tr>Női</tr>
-          <tr>
-            <th>Alapszolgáltatás</th>
-            <th>Rövid</th>
-            <th>Félhosszú</th>
-            <th>Hosszú</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Mosás+Szárítás</th>
-            <td>5000 HUF</td>
-            <td>6000 HUF</td>
-            <td>8000 HUF</td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
+    <section className="prices--container"></section>
   )
 }
 

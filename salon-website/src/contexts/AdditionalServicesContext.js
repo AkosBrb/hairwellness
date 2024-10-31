556fa19b8af715b693667ae3a62b0f2a7212c8b6
akos borbely <macos.borbely@gmail.com>
2024-10-31 07:48:26 +0100

    extra services and additional services context created

---
+++ salon-website/src/contexts/AdditionalServicesContext.js
@@ -0,0 +1,14 @@
+import { createContext, useEffect, useState } from "react";
+import getAdditionalServices from "../services/fetchAdditionalServices";
+
+export const AdditionalServicesContext = createContext();
+
+export function AdditionalServicesProvider({ children }) {
+  const [additionalServices, setAdditionalServices] = useState([]);
+
+  useEffect(() => {
+    getAdditionalServices().then(data => setAdditionalServices(data));
+  }, [])
+
+  return <AdditionalServicesContext.Provider value={{ additionalServices, setAdditionalServices }}>{children}</AdditionalServicesContext.Provider>
+}
---
+++ salon-website/src/contexts/ExtraServicesContext.js
@@ -0,0 +1,14 @@
+import { createContext, useEffect, useState } from "react";
+import getExtraServices from "../services/fetchExtraServices";
+
+export const ExtraServicesContext = createContext();
+
+export function ExtraServicesProvider({ children }) {
+  const [extraServices, setExtraServices] = useState([]);
+
+  useEffect(() => {
+    getExtraServices().then(data => setExtraServices(data));
+  }, [])
+
+  return <ExtraServicesContext.Provider value={{ extraServices, setExtraServices }}>{children}</ExtraServicesContext.Provider>
+}

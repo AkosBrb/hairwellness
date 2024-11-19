bdff8c5f85db6fd3658044e7f56a647a7d63e417
akos borbely <macos.borbely@gmail.com>
2024-11-19 06:42:35 +0100

    service cards component implement

--- salon-website/src/components/Main.js
+++ salon-website/src/components/Main.js
@@ -1,10 +1,12 @@
 import "../Style.css";
 import Introduction from "./Introduction";
+import ServiceCards from "./ServiceCards";
 
 function Main() {
     return (
         <section className="landing--page--container">
             <Introduction />
+            <ServiceCards />
         </section>
     )
 }

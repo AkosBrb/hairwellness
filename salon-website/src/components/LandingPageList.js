8839fbad3d750e753c27526a0b87b802a8e5b0b8
akos borbely <macos.borbely@gmail.com>
2024-11-20 07:14:56 +0100

    landing page list created

---
+++ salon-website/src/components/LandingPageList.js
@@ -0,0 +1,40 @@
+import { MdFileDownloadDone } from "react-icons/md";
+
+function LandingPageList() {
+  return (
+    <div className="landing--page--list--container">
+      <div className="title">
+        <h2>Amit a Hajwellness nyújt neked:</h2>
+        <div className="underline"></div>
+      </div>
+      <ul>
+        <li>
+          <MdFileDownloadDone className="landing--page--list--icon" size={50} />
+          <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat aenean nullam tortor morbi potenti aenean. Suspendisse tincidunt vestibulum aliquet molestie varius.</p>
+        </li>
+        <li>
+          <MdFileDownloadDone className="landing--page--list--icon" size={50} />
+          <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat aenean nullam tortor morbi potenti aenean. Suspendisse tincidunt vestibulum aliquet molestie varius.</p>
+        </li>
+        <li>
+          <MdFileDownloadDone className="landing--page--list--icon" size={50} />
+          <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat aenean nullam tortor morbi potenti aenean. Suspendisse tincidunt vestibulum aliquet molestie varius.</p>
+        </li>
+        <li>
+          <MdFileDownloadDone className="landing--page--list--icon" size={50} />
+          <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat aenean nullam tortor morbi potenti aenean. Suspendisse tincidunt vestibulum aliquet molestie varius.</p>
+        </li>
+        <li>
+          <MdFileDownloadDone className="landing--page--list--icon" size={50} />
+          <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat aenean nullam tortor morbi potenti aenean. Suspendisse tincidunt vestibulum aliquet molestie varius.</p>
+        </li>
+        <li>
+          <MdFileDownloadDone className="landing--page--list--icon" size={50} />
+          <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat aenean nullam tortor morbi potenti aenean. Suspendisse tincidunt vestibulum aliquet molestie varius.</p>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default LandingPageList;

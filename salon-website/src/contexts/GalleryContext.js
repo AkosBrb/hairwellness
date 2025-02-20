b24c89b8bdad19760ef805b32e3cdaafbeef78aa
akos borbely <macos.borbely@gmail.com>
2025-02-20 20:10:12 +0100

    gallery context setup

--- salon-website/src/contexts/GalleryContext.js
+++ salon-website/src/contexts/GalleryContext.js
@@ -1,9 +1,14 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import fetchGallery from "../services/fetchPhotos";
 
 export const GalleryContext = createContext();
 
 export function GalleryProvider({ children }) {
   const [gallery, setGallery] = useState([]);
+
+  useEffect(() => {
+    fetchGallery(setGallery)
+  }, [])
 
   return <GalleryContext.Provider value={{ gallery, setGallery }}>{children}</GalleryContext.Provider>
 }

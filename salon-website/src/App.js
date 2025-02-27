06d2c06ee2e30b96400074c6e32909fee6ac074b
akos borbely <macos.borbely@gmail.com>
2025-02-27 18:49:43 +0100

    galleryContext deleted

--- salon-website/src/App.js
+++ salon-website/src/App.js
@@ -4,18 +4,15 @@ import Footer from "./components/Footer";
 import { ServicesProvider } from './contexts/ServicesContext';
 import { AdditionalServicesProvider } from './contexts/AdditionalServicesContext';
 import { ExtraServicesProvider } from './contexts/ExtraServicesContext';
-import { GalleryProvider } from './contexts/GalleryContext';
 
 function ContentLayout() {
   return (
     <ExtraServicesProvider>
       <AdditionalServicesProvider>
         <ServicesProvider>
-          <GalleryProvider>
-            <Header />
-            <Outlet />
-            <Footer />
-          </GalleryProvider>
+          <Header />
+          <Outlet />
+          <Footer />
         </ServicesProvider>
       </AdditionalServicesProvider>
     </ExtraServicesProvider>

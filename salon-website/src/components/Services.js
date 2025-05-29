d58191eb780c54b82c3c215c9ea9554da09571e6
AkosBrb <macos.borbely@gmail.com>
2025-05-29 18:54:27 +0200

    div changed to section

--- salon-website/src/components/Services.js
+++ salon-website/src/components/Services.js
@@ -2,10 +2,10 @@ import ServicesComponent from "./ServicesComponent"
 import ServicesPriceTable from "./ServicesPriceTable"
 function Services() {
     return (
-        <>
+        <section className="services--page">
             <ServicesComponent />
             <ServicesPriceTable />
-        </>
+        </section>
     )
 }
 
